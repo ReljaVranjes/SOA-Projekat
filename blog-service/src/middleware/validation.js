880d6c51f920eb453899ff4fe9bcb41fc395efe0
Adrijan Radjevic <adrijan.radjevic@gmail.com>
2025-07-16 20:33:25 +0200

    Extract user ID from token for blog and comment creation

--- blog-service/src/middleware/validation.js
+++ blog-service/src/middleware/validation.js
@@ -3,7 +3,6 @@ const { body, validationResult } = require('express-validator');
 exports.validateBlog = [
   body('title').notEmpty().withMessage('Title is required'),
   body('description').notEmpty().withMessage('Description is required'),
-  body('authorId').notEmpty().withMessage('Author ID is required'),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -14,7 +13,6 @@ exports.validateBlog = [
 ];
 
 exports.validateComment = [
-  body('userId').notEmpty().withMessage('User ID is required'),
   body('text').notEmpty().withMessage('Comment text is required'),
   (req, res, next) => {
     const errors = validationResult(req);
