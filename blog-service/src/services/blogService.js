e01dd890c773b1c9ea32f96099a9605c39d63926
Adrijan Radjevic <adrijan.radjevic@gmail.com>
2025-07-16 15:01:33 +0200

    Implemented like feature for blogs, fixed comment routes

--- blog-service/src/services/blogService.js
+++ blog-service/src/services/blogService.js
@@ -13,8 +13,22 @@ const getBlogById = async (id) => {
   return await Blog.findById(id);
 };
 
+const likeBlog = async (id, userId) => {
+  const blog = await Blog.findById(id);
+  if (!blog) throw new Error('Blog not found');
+
+  if (blog.likes.includes(userId)) {
+    blog.likes = blog.likes.filter((like) => like !== userId);
+  } else {
+    blog.likes.push(userId);
+  } 
+  return await blog.save();
+};
+
+
 module.exports = {
   createBlog,
   getAllBlogs,
   getBlogById,
+  likeBlog
 };
